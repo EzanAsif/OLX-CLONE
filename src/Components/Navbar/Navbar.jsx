eac560b7b09731ee95ddcc156bc764449d8e1dd8
Siddiq <siddiqahmed882@gmail.com>
2021-04-21 00:38:22 +0500

    fixed responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import './styles.css';
 import SellButton from '../../assets/olxSellButton-01.png';
 import { Link } from "react-router-dom";
-import {Auth} from '../../Auth/Auth'
+import { Auth } from '../../Auth/Auth'
 import firebase from "firebase";
-import { useSelector , useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import LoginButton from '../LoginPopup/LoginPopup'
 import AdPosting from '../../Pages/AdPosting'
 // import SellButton from '../sellButton/SellButton'
@@ -26,47 +26,47 @@ const NavbarUpper = () => {
 
     let Comminguser = useSelector(state => state.userData);
     // console.log("COMMING FROM REDUCER =>", Comminguser)
-  
-    const dispatch = useDispatch();
-  
-    const GoogleLogin = () => {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth()
-              .signInWithPopup(provider)
-              .then((result) => {
-                  /** @type {firebase.auth.OAuthCredential} */
-                  var credential = result.credential;
-                  var token = credential.accessToken;
-                  var user = result.user;
-                  console.log(user.displayName, user.email, user.photoURL);
-  
-                  let userDetails = {
-                      name : user.displayName,
-                      email : user.email,
-                      photoUrl : user.photoURL
-                  }
-  
-                  console.log("USER DETAILS", userDetails);
-                  
-                  dispatch({type: "ADD_USER", payload:userDetails})
-  
-          
-              }).catch((error) => {
-                  // Handle Errors here.
-                  var errorCode = error.code;
-                  var errorMessage = error.message;
-                  // The email of the user's account used.
-                  var email = error.email;
-                  // The firebase.auth.AuthCredential type that was used.
-                  var credential = error.credential;
-                  // ...
-              });
-  
-              
-          }
-         
 
-    return(
+    const dispatch = useDispatch();
+
+    const GoogleLogin = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log(user.displayName, user.email, user.photoURL);
+
+                let userDetails = {
+                    name: user.displayName,
+                    email: user.email,
+                    photoUrl: user.photoURL
+                }
+
+                console.log("USER DETAILS", userDetails);
+
+                dispatch({ type: "ADD_USER", payload: userDetails })
+
+
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+
+
+    }
+
+
+    return (
         <div className="navbar_upper">
             <div className="logo">
                 <Link to="/">
@@ -75,38 +75,38 @@ const NavbarUpper = () => {
             </div>
             <div className="city_picker">
                 <div className="form">
-                    <select name="city_picker" placeholder = "Paksitan">
+                    <select name="city_picker" placeholder="Paksitan">
                         <option value="Karachi">Karachi</option>
                         <option value="Lahore">Lahore</option>
                         <option value="Islamabad">Islamabad</option>
                         <option value="Multan">Multan</option>
                     </select>
-               
+
                 </div>
             </div>
             <div className="search_box">
-                <input type="text" placeholder = "Find cars, mobiles and more ...."/>
+                <input type="text" placeholder="Find cars, mobiles and more ...." />
                 <SearchIcon />
             </div>
-            
+
             {
-                Comminguser.Ustate.name 
-                ? 
+                Comminguser.Ustate.name
+                    ?
                     <div className="authenticated">
                         <div className="user">
-                            <img src={Comminguser.Ustate.photoUrl} alt=""/>
+                            <img src={Comminguser.Ustate.photoUrl} alt="" />
                             <h4>{Comminguser.Ustate.name}</h4>
                         </div>
                         <div className="sellPage">
-                            <Link to = {"/createAd"}>
-                                <img src= {SellButton}alt="SellButton"/>
+                            <Link to={"/createAd"}>
+                                <img src={SellButton} alt="SellButton" />
                             </Link>
                         </div>
                     </div>
-                :
-                <div className="buttons_notAuthenticated">
-                    <LoginButton />
-                </div>
+                    :
+                    <div className="buttons_notAuthenticated">
+                        <LoginButton />
+                    </div>
             }
         </div>
     )
@@ -114,143 +114,143 @@ const NavbarUpper = () => {
 
 
 const NavbarBottom = () => {
-    return(
+    return (
         <div className="bottom-nav">
             <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                ALL CATEGORIES
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    ALL CATEGORIES
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Vehicle</Dropdown.Item>
-                <ul>
-                    <a href="#home"><li>
-                    Cars
+                <Dropdown.Menu>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Vehicle</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Cars
                     </li></a>
-                    <a href="#home"><li>
-                    Tractors
+                            <a href="#home"><li>
+                                Tractors
                     </li></a>
-                    <a href="#home"><li>
-                    Bikes
+                            <a href="#home"><li>
+                                Bikes
                     </li></a>
-                </ul>
-                </div>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Animals</Dropdown.Item>
-                <ul>
-                <a href="#home"><li>
-                    Cats
+                        </ul>
+                    </div>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Animals</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Cats
                     </li></a>
-                    <a href="#home"><li>
-                    Dogs
+                            <a href="#home"><li>
+                                Dogs
                     </li></a>
-                    <a href="#home"><li>
-                    Parrots
+                            <a href="#home"><li>
+                                Parrots
                     </li></a>
-                </ul>
-                </div>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Fashion And Beauty</Dropdown.Item>
-                <ul>
-                <a href="#home"><li>
-                    Wedding
+                        </ul>
+                    </div>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Fashion And Beauty</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Wedding
                     </li></a>
-                    <a href="#home"><li>
-                    Watches
+                            <a href="#home"><li>
+                                Watches
                     </li></a>
-                    <a href="#home"><li>
-                    Clothes
+                            <a href="#home"><li>
+                                Clothes
                     </li></a>
-                    <a href="#home"><li>
-                    Make Up
+                            <a href="#home"><li>
+                                Make Up
                     </li></a>
-                    <a href="#home"><li>
-                    Jewellery
+                            <a href="#home"><li>
+                                Jewellery
                     </li></a>
-                    <a href="#home"><li>
-                    Accesories
+                            <a href="#home"><li>
+                                Accesories
                     </li></a>
-                </ul>
-                </div>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Mobiles</Dropdown.Item>
-                <ul>
-                <a href="#home"><li>
-                    Mobile Phones
+                        </ul>
+                    </div>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Mobiles</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Mobile Phones
                     </li></a>
-                    <a href="#home"><li>
-                    Accessories
+                            <a href="#home"><li>
+                                Accessories
                     </li></a>
-                    <a href="#home"><li>
-                    Tablets
+                            <a href="#home"><li>
+                                Tablets
                     </li></a>
-                </ul>
-                </div>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Furniture And Home Decoration</Dropdown.Item>
-                <ul>
-                <a href="#home"><li>
-                    Office Household items
+                        </ul>
+                    </div>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Furniture And Home Decoration</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Office Household items
                     </li></a>
-                    <a href="#home"><li>
-                    Office Furniture
+                            <a href="#home"><li>
+                                Office Furniture
                     </li></a>
-                    <a href="#home"><li>
-                    Curtains And Blinds
+                            <a href="#home"><li>
+                                Curtains And Blinds
                     </li></a>
-                    <a href="#home"><li>
-                    Rugs And Carpets
+                            <a href="#home"><li>
+                                Rugs And Carpets
                     </li></a>
-                    <a href="#home"><li>
-                    Painting And Mirrors
+                            <a href="#home"><li>
+                                Painting And Mirrors
                     </li></a>
-                    <a href="#home"><li>
-                    Garden and Outdoor
+                            <a href="#home"><li>
+                                Garden and Outdoor
                     </li></a>
-                </ul>
-                </div>
-                <div className="dropdown-item-cont">
-                <Dropdown.Item href="#">Jobs</Dropdown.Item>
-                <ul>
-                <a href="#home"><li>
-                    Other Jobs
+                        </ul>
+                    </div>
+                    <div className="dropdown-item-cont">
+                        <Dropdown.Item href="#">Jobs</Dropdown.Item>
+                        <ul>
+                            <a href="#home"><li>
+                                Other Jobs
                     </li></a>
-                    <a href="#home"><li>
-                    Part-time
+                            <a href="#home"><li>
+                                Part-time
                     </li></a>
-                    <a href="#home"><li>
-                    Domestic Staff
+                            <a href="#home"><li>
+                                Domestic Staff
                     </li></a>
-                    <a href="#home"><li>
-                    Medical
+                            <a href="#home"><li>
+                                Medical
                     </li></a>
-                    <a href="#home"><li>
-                    Manufacturing
+                            <a href="#home"><li>
+                                Manufacturing
                     </li></a>
-                    <a href="#home"><li>
-                    Human Resources
+                            <a href="#home"><li>
+                                Human Resources
                     </li></a>
-                </ul>
-                </div>
-            </Dropdown.Menu>
+                        </ul>
+                    </div>
+                </Dropdown.Menu>
             </Dropdown>
             <div className="popular-categories">
-        <ul>
-            <Link to = {'/categories/Mobile_Phones'}><li>Mobile Phones</li></Link>
-            <Link to = {'/categories/Laptops'}><li>Laptops</li></Link>
-            <Link to = {'/categories/Motorcycles'}><li>Motorcycles</li></Link>
-            <Link to = {'/categories/TV'}><li>TVs</li></Link>
-            <Link to = {'/categories/Land_And_Plots'}><li>Land And Plots</li></Link>
-        </ul>
-        </div>
+                <ul>
+                    <Link to={'/categories/Mobile_Phones'}><li>Mobile Phones</li></Link>
+                    <Link to={'/categories/Laptops'}><li>Laptops</li></Link>
+                    <Link to={'/categories/Motorcycles'}><li>Motorcycles</li></Link>
+                    <Link to={'/categories/TV'}><li>TVs</li></Link>
+                    <Link to={'/categories/Land_And_Plots'}><li>Land And Plots</li></Link>
+                </ul>
+            </div>
         </div>
     )
 }
 
 const NavbarM = () => {
     return (
-        <div className = "navbar_main">
+        <div className="navbar_main">
             <NavbarUpper />
             <NavbarBottom />
         </div>
